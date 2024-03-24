61b81be8edefa066e8f8ff5aae14dcfc9b7aae7a
PeterHassaballah <peter.hassaballah@gmail.com>
2024-03-24 14:00:48 +0200

    [feat] main router file

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,6 +1,17 @@
 import { Router } from "express";
-
+import authRoute from './auth';
+import userRoute from './users';
 const router = Router();
-const defaultRoutes=[];
+const defaultRoutes=[{
+    path: "/auth",
+    route: authRoute,
+  },
+  {
+    path: "/users",
+    route: userRoute,
+  }];
+defaultRoutes.forEach((route) => {
+    router.use(route.path, route.route);
+  });
 // export default router;
 export { router as indexRoute };
