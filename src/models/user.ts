523af0781282ec79f0046f0f9c212781e72d943b
PeterHassaballah <peter.hassaballah@gmail.com>
2024-03-24 14:30:04 +0200

    [feat] create user model and interface

---
+++ src/models/user.ts
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+import { IUser } from 'src/types/user.interfaces';
+const userSchema = new mongoose.Schema<IUser>(
+    {
+      name: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+      },
+      password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 8,
+        private: true, // used by the toJSON plugin
+      },
+      age:{
+
+      },
+      active: {
+        type: Boolean,
+        default: false,
+      },
+      lastLogin:{
+          type:String,
+      }
+    },
+    
+  );
+  
+const User = mongoose.model<IUser>('User', userSchema);
+
+export default User;
