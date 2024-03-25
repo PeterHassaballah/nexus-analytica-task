ff63dcfed4cfe0040f073e56018ba217cad94bc1
PeterHassaballah <peter.hassaballah@gmail.com>
2024-03-25 18:53:51 +0200

    [feat] add upload features

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,17 @@
 import express, { json, urlencoded } from "express";
-
+import bodyParser from "body-parser";
 import compression from "compression";
 import cors from "cors";
 import { indexRoute } from "./routes/v1/index";
-
+console.log("hello world")
 const app = express();
 
 // parse json request body
 app.use(json());
 // gzip compression
 app.use(compression());
+// body parser
+app.use(bodyParser.urlencoded({extended:true}));
 // enable cors
 app.use(cors());
 // parse urlencoded request body
